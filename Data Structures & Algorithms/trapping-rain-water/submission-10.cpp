d97f8576edc2a617293c5f4f9afeeccc6501a5ff
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-11 18:31:39 +0500

    Add: trapping-rain-water - submission-10

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-10.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int count = 0;
+        vector<int> pref(height.size(), 0);
+        vector<int> suf(height.size(), 0);
+        int lMax = 0;
+        int rMax = 0;
+        for(int i = 0; i < height.size(); i++){
+            if(i == 0){
+                pref[i] = 0;
+                lMax = max(lMax, height[i]);
+                continue;
+            }
+            lMax = max(lMax, height[i]);
+            pref[i] = lMax;
+        }
+
+        for(int i = height.size() - 1; i >= 0; i--){
+            if(i == height.size() - 1){
+                suf[i] = 0;
+                rMax = max(rMax, height[i]);
+                continue;
+            }
+            rMax = max(rMax, height[i]);
+            suf[i] = rMax;
+        }
+
+        for(int i = 0; i < height.size(); i++){
+            int h = min(pref[i], suf[i]);
+            int water = h - height[i];
+            if(water > 0) count+=water;
+        }
+        return count;
+    }
+};
