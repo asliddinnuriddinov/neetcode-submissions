34ba64b4ba8ed67fb51f82a683544eb074220cd5
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-21 06:12:08 +0500

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> sub;
+        vector<int> seen(nums.size(), false);
+
+        dfs(nums, sub, seen);
+
+        return ans;
+    }
+
+    void dfs(vector<int>& nums, vector<int>& sub, vector<int>& seen){
+        if(sub.size() == nums.size()){
+            ans.push_back(sub);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(!seen[i]){
+                seen[i] = true;
+
+                sub.push_back(nums[i]);
+                dfs(nums, sub, seen);
+                sub.pop_back();
+
+                seen[i] = false;
+            }
+        }
+    }
+};
