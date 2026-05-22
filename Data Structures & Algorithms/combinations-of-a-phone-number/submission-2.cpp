1db38f265f681cbdf6a85a07668b3313294c9107
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-22 07:59:55 +0500

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    char letters[8][4] = {
+        {'a', 'b', 'c', '\0'},
+        {'d', 'e', 'f', '\0'},
+        {'g', 'h', 'i', '\0'},
+        {'j', 'k', 'l', '\0'},
+        {'m', 'n', 'o', '\0'},
+        {'p', 'q', 'r', 's'},
+        {'t', 'u', 'v', '\0'},
+        {'w', 'x', 'y', 'z'}
+    };
+public:
+    vector<string> ans;
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0) return {};
+        string sub;
+
+        dfs(digits, sub, 0);
+
+        return ans;
+    }
+
+    void dfs(string & digits, string &sub, int i){
+        if(sub.length() == digits.length()){
+            ans.push_back(sub);
+            return;
+        }
+        for(int p = 0; p < 4; p++){
+            if(letters[(digits[i] - '0') - 2][p] == '\0') continue;
+            sub.push_back(letters[(digits[i] - '0') - 2][p]);
+            dfs(digits, sub, i + 1);
+            sub.pop_back();
+        }
+    }
+};
