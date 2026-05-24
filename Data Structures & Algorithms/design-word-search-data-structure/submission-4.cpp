730f436c652656759909f349167c0a3849f2a3ea
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-24 06:23:34 +0500

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.cpp
@@ -0,0 +1,53 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool endOfWord;
+    TrieNode(){
+        for(int i = 0; i < 26; i++){
+            children[i] = nullptr;
+        }
+        endOfWord = false;
+    }
+};
+
+class WordDictionary {
+    TrieNode* root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* curr = root;
+        for(char &c : word){
+            int i = c - 'a';
+            if(curr->children[i] == NULL){
+                curr->children[i] = new TrieNode();
+            }
+
+            curr = curr->children[i];
+        }
+        curr->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        return dfs(word, 0, root);
+    }
+
+    bool dfs(string &word, int i, TrieNode* curr){
+        for(int j = i; j < word.length(); j++){
+            char &ch = word[j];
+            if(ch == '.'){
+                for(TrieNode* child : curr->children){
+                    if(child != NULL && dfs(word, j + 1, child)) return true;
+                }
+                return false;
+            }
+            else{
+                if(curr->children[ch - 'a'] == NULL) return false;
+                curr = curr->children[ch - 'a'];
+            }
+        }
+        return curr->endOfWord;
+    }
+};
