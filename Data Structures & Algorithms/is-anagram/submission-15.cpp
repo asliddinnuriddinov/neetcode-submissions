e84c88bd78afc253f92790d9671bad2c98ace43a
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-26 10:13:20 +0500

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        int count[26] = {};
+
+        for(int i = 0; i < s.length(); i++){
+            count[s[i] - 'a']+=1;
+            count[t[i] - 'a']-=1;
+        }
+
+        for(int &c : count){
+            if(c != 0) return false;
+        }
+        return true;
+    }
+};
