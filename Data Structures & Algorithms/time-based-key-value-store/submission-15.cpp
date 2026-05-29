671cac87f74a61377478b0f0e416d048db0c589b
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-29 10:16:13 +0500

    Add: time-based-key-value-store - submission-15

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-15.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+    unordered_map<string, vector<pair<string, int>>> mp {};
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        vector<pair<string, int>> arr = mp[key];
+        int l = 0, r = arr.size() - 1;
+        string res = "";
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(arr[mid].second == timestamp) return arr[mid].first;
+            if(arr[mid].second <= timestamp){
+                res = arr[mid].first;
+                l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+        return res;
+    }
+};
