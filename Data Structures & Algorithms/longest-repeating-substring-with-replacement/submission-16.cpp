7f1d43047bfc044e6b6745bd7c07d3ce68fc4a63
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-05-30 09:11:16 +0500

    Add: longest-repeating-substring-with-replacement - submission-16

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-16.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, most = 0, res = 0;
+        unordered_map<char, int> mp {};
+
+        for(int r = 0; r < s.length(); r++){
+            mp[s[r]]+=1;
+            most = max(most, mp[s[r]]);
+
+            while(r - l + 1 - most > k){
+                mp[s[l]]-=1;
+                l++;
+                for(const auto &[k, v] : mp){
+                    most = max(most, v);
+                }
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
