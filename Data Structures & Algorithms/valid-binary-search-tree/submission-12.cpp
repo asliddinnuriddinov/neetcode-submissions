a43966db9cb642d2d2d048c796408b448b381548
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-06-01 17:54:31 +0500

    Add: valid-binary-search-tree - submission-12

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-12.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        queue<tuple<TreeNode*, long, long>> qu {};
+        qu.push({root, LONG_MIN, LONG_MAX});
+        while(!qu.empty()){
+            TreeNode* curr = get<0>(qu.front());
+            long minVal = get<1>(qu.front());
+            long maxVal = get<2>(qu.front());
+            qu.pop();
+            if(curr->val <= minVal || curr->val >= maxVal) return false;
+            if(curr->left) qu.push({curr->left, minVal, curr->val});
+            if(curr->right) qu.push({curr->right, curr->val, maxVal});
+        }
+        return true;
+    }
+};
