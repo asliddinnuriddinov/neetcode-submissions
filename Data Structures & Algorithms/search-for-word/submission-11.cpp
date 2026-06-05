7843289405ec7af063cc0e0572acf6bc434de319
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-06-05 19:44:35 +0500

    Add: search-for-word - submission-11

---
+++ Data Structures & Algorithms/search-for-word/submission-11.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ROWS, COLS;
+    bool exist(vector<vector<char>>& board, string word) {
+        ROWS = board.size() - 1, COLS = board[0].size() - 1;
+
+        for(int r = 0; r <= ROWS; r++){
+            for(int c = 0; c <= COLS; c++){
+                if(board[r][c] == word[0] && dfs(board, word, r, c, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string& word, int r, int c, int i){
+        if(i == word.size()) return true;
+
+        if(r < 0 || r > ROWS || c < 0 || c > COLS || board[r][c] != word[i]){
+            return false;
+        }
+
+        board[r][c] = '#';
+        bool result = dfs(board, word, r + 1, c, i + 1) ||
+                      dfs(board, word, r - 1, c, i + 1) ||
+                      dfs(board, word, r, c + 1, i + 1) ||
+                      dfs(board, word, r, c - 1, i + 1);
+
+        board[r][c] = word[i];
+
+        return result;
+    }
+};
