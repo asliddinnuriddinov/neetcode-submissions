7d9dc224588e9bdfa0e3bfbbf85aeeee40551977
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-06-05 19:29:07 +0500

    Add: subsets-ii - submission-4

---
+++ Data Structures & Algorithms/subsets-ii/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans {};
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> sub {};
+        sort(nums.begin(), nums.end());
+
+        dfs(nums, sub, 0);
+        return ans;
+    }
+
+    void dfs(vector<int>& nums, vector<int>& sub, int i){
+        if(i == nums.size()){
+            ans.push_back(sub);
+            return;
+        }
+        sub.push_back(nums[i]);
+        dfs(nums, sub, i + 1);
+        sub.pop_back();
+
+        while(i != nums.size() - 1 && nums[i] == nums[i + 1]) i++;
+        dfs(nums, sub, i + 1);
+    }
+};
