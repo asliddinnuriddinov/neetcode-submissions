e2b8da64c956644afe36c667a3acaaf0505a0aea
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-08-08 06:55:13 +0500

    Add: daily-temperatures - submission-9

---
+++ Data Structures & Algorithms/daily-temperatures/submission-9.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temps) {
+        stack<int> st {};
+        vector<int> res(temps.size(), 0);
+
+        for(int i = 0; i < temps.size(); i++){
+            if(st.empty() || temps[i] < temps[st.top()]){
+                st.push(i);
+            }
+            else{
+                while(!st.empty() && temps[i] > temps[st.top()]){
+                    res[st.top()] = i - st.top();
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return res;
+    }
+};
