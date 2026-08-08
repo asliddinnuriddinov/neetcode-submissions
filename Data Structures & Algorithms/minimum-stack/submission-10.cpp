3276d4bb154098f57f548cdff6d034217578796a
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-08-08 06:38:18 +0500

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    stack<int> st {};
+    stack<int> minSt {};
+    MinStack() {
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty() || val <= minSt.top()){
+            minSt.push(val);
+        }
+    }
+    
+    void pop() {
+        if(minSt.top() == st.top()) minSt.pop();
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
