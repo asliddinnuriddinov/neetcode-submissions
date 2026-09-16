7e2ce2655fb5a4ff88354492cd4e005e7946628a
asliddinnuriddinov <134066001+asliddinnuriddinov@users.noreply.github.com>
2026-09-16 17:30:40 +0500

    Add: subsets - submission-9

---
+++ Data Structures & Algorithms/subsets/submission-9.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> res {};
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> sub {};
+        dfs(nums, sub, 0);
+        return res;
+    }
+    void dfs(vector<int>& nums, vector<int>& sub, int i){
+        if(i == nums.size()){
+            res.push_back(sub);
+            return;
+        }
+        sub.push_back(nums[i]);
+        dfs(nums, sub, i + 1);
+        sub.pop_back();
+        dfs(nums, sub, i + 1);
+    }
+};
